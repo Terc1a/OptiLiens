8b4c6e3d07637b75370eba1bf6bb415892f80560
tercia <gamenixcoc@gmail.com>
2025-07-25 00:01:53 +0500

    Дописал больше роутеров под все нужные мне сервисы + обновил сбор краткой статистики для homepage

--- src/routes.py
+++ src/routes.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 router = APIRouter()
 
-@router.get("/")            # корень теперь здесь
+@router.get("/")            # mirroring для hikariplus.ru
 async def analyze(request: Request):
     """Главный обработчик корневого пути /"""
     client_ip = (
@@ -42,6 +42,114 @@ async def analyze(request: Request):
     return {"message": "ok"}
 
 
+@router.get("/wishes")            # mirroring для wish.hikariplus.ru
+async def analyze(request: Request):
+    """Главный обработчик корневого пути /"""
+    client_ip = (
+        request.headers.get("x-real-ip") or
+        request.headers.get("x-forwarded-for") or
+        request.client.host
+    )
+    ua = request.headers.get("user-agent", "")
+    short_ua = ua.split()[0] if ua else "Unknown"
+    is_mobile = request.headers.get("sec-ch-ua-platform") == '"Android"'
+
+    logger.info(
+        f"{request.method} {client_ip} mobile={is_mobile} UA={short_ua}"
+    )
+
+    with get_cursor() as (cur, _):
+        # Есть ли такой IP?
+        cur.execute(
+            "SELECT name FROM wishes WHERE addr=%s LIMIT 1",
+            (client_ip,)
+        )
+        row = cur.fetchone()
+        name = row[0] if row else str(uuid.uuid4())
+
+        # Записываем визит
+        cur.execute(
+            """INSERT INTO wishes
+               (addr, name, method, timed, is_mobile, user_agent)
+               VALUES (%s, %s, %s, %s, %s, %s)""",
+            (client_ip, name, request.method, datetime.now(), is_mobile, short_ua)
+        )
+
+    return {"message": "ok"}
+
+
+@router.get("/manage")            # mirroring для manage.hikariplus.ru
+async def analyze(request: Request):
+    """Главный обработчик корневого пути /"""
+    client_ip = (
+        request.headers.get("x-real-ip") or
+        request.headers.get("x-forwarded-for") or
+        request.client.host
+    )
+    ua = request.headers.get("user-agent", "")
+    short_ua = ua.split()[0] if ua else "Unknown"
+    is_mobile = request.headers.get("sec-ch-ua-platform") == '"Android"'
+
+    logger.info(
+        f"{request.method} {client_ip} mobile={is_mobile} UA={short_ua}"
+    )
+
+    with get_cursor() as (cur, _):
+        # Есть ли такой IP?
+        cur.execute(
+            "SELECT name FROM manage WHERE addr=%s LIMIT 1",
+            (client_ip,)
+        )
+        row = cur.fetchone()
+        name = row[0] if row else str(uuid.uuid4())
+
+        # Записываем визит
+        cur.execute(
+            """INSERT INTO manage
+               (addr, name, method, timed, is_mobile, user_agent)
+               VALUES (%s, %s, %s, %s, %s, %s)""",
+            (client_ip, name, request.method, datetime.now(), is_mobile, short_ua)
+        )
+
+    return {"message": "ok"}
+
+
+@router.get("/blog")            # mirroring для manage.hikariplus.ru
+async def analyze(request: Request):
+    """Главный обработчик корневого пути /"""
+    client_ip = (
+        request.headers.get("x-real-ip") or
+        request.headers.get("x-forwarded-for") or
+        request.client.host
+    )
+    ua = request.headers.get("user-agent", "")
+    short_ua = ua.split()[0] if ua else "Unknown"
+    is_mobile = request.headers.get("sec-ch-ua-platform") == '"Android"'
+
+    logger.info(
+        f"{request.method} {client_ip} mobile={is_mobile} UA={short_ua}"
+    )
+
+    with get_cursor() as (cur, _):
+        # Есть ли такой IP?
+        cur.execute(
+            "SELECT name FROM blog WHERE addr=%s LIMIT 1",
+            (client_ip,)
+        )
+        row = cur.fetchone()
+        name = row[0] if row else str(uuid.uuid4())
+
+        # Записываем визит
+        cur.execute(
+            """INSERT INTO blog
+               (addr, name, method, timed, is_mobile, user_agent)
+               VALUES (%s, %s, %s, %s, %s, %s)""",
+            (client_ip, name, request.method, datetime.now(), is_mobile, short_ua)
+        )
+
+    return {"message": "ok"}
+
+
 @router.get("/stats")
 async def stats():
     """JSON-эндпоинт для дашборда"""
@@ -51,23 +159,22 @@ async def stats():
         rc = cur.fetchone()[0]
 
         # уникальные адреса и количество
-        cur.execute("SELECT addr, COUNT(*) AS c FROM hikariplus GROUP BY addr")
-        uc_rows = cur.fetchall()
-        uc = len(uc_rows)
-        test = {ip: cnt for ip, cnt in uc_rows}
+        cur.execute("SELECT COUNT(*) AS c FROM hikariplus GROUP BY addr")
+        todo_count = cur.fetchall()
+
 
         # уникальные адреса и количество2
-        cur.execute("SELECT DISTINCT(addr), COUNT(*) AS req_count FROM hikariplus GROUP BY addr ORDER BY req_count DESC limit 10")
-        uco_rows = cur.fetchall()
-        uco = len(uc_rows)
-        uniq= {ip: cnt for ip, cnt in uc_rows}
+        cur.execute("SELECT COUNT(*) AS c FROM wishes GROUP BY addr")
+        wish_count = cur.fetchall()
 
-        # мобильные
-        cur.execute(
-            "SELECT addr FROM hikariplus WHERE is_mobile='True' GROUP BY addr"
-        )
-        mc_rows = cur.fetchall()
-        mc = len(mc_rows)
-        mcounter = mc_rows
 
-    return {"rc": rc, "uc": uc, "test": test, "mc": mc, "mcounter": mcounter, "uco": uco, "uniq": uniq}
+        # уникальные адреса и количество3
+        cur.execute("SELECT COUNT(*) AS c FROM manage GROUP BY addr")
+        manage_count = cur.fetchall()
+
+
+        # уникальные адреса и количество3
+        cur.execute("SELECT COUNT(*) AS c FROM blog GROUP BY addr")
+        blog_count = cur.fetchall()
+
+    return {"todo": todo_count, "wish": wish_count, "manage": manage_count, "blog": blog_count}
